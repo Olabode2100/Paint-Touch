727bfef967be5be19c95abe6ea8b3086c59cb99f
Olabode Aluko <75957716+Olabode2100@users.noreply.github.com>
2021-02-08 19:35:15 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -48,16 +48,16 @@ canvas.addEventListener("touchmove",my_touchmove);
 function my_touchmove(e){
     current_position_of_touch_x=e.touches[0].clientX-canvas.offsetLeft;
      current_position_of_touch_y=e.touches[0].clientY-canvas.offsetTop;
-   if (mouseEvent="mousedown"){
+
        ctx.beginPath();
 ctx.strokeStyle=color;
 ctx.lineWidth=width_of_line;
-ctx.moveTo(last_position_of_x, last_position_of_y);
-ctx.lineTo(current_position_of_mouse_x,current_position_of_mouse_y);
+ctx.moveTo(last_position_of_touch_x, last_position_of_touch_y);
+ctx.lineTo(current_position_of_touch_x,current_position_of_touch_y);
 ctx.stroke();
-   }
-    last_position_of_x=current_position_of_mouse_x;
-    last_position_of_y=current_position_of_mouse_y;
+   
+    last_position_of_touch_x=current_position_of_touch_x;
+    last_position_of_touch_y=current_position_of_touch_y;
 }
 
 function clearArea(){
